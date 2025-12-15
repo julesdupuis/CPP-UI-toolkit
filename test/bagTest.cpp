747f374809df92ec936ecfae7bcffc265392116e
julesdupuis <julesfelixdupuis@gmail.com>
2025-12-15 16:30:43 +0100

    Bag tests for dummy class

--- test/bagTest.cpp
+++ test/bagTest.cpp
@@ -1,17 +1,22 @@
 #include "bagTest.hpp"
 #include <cppunit/TestAssert.h>
+#include <string>
 
-void BagTest::testInit(){
+std::ostream& operator<<(std::ostream& stream, const Dummyclass& dummy){
+    return stream << dummy.getName();
+}
+
+void BagTest::testIntInit(){
     Bag<int> bagA({5,7,74,12});
     Bag<int> bagB({5,7,74,12});
     CPPUNIT_ASSERT(&bagA != &bagB);
     CPPUNIT_ASSERT_EQUAL(bagA, bagB);
-    
+
     Bag<int> bagC({5,7,74,12, 34});
     CPPUNIT_ASSERT(!(bagA == bagC));
 }
 
-void BagTest::testAdd(){
+void BagTest::testIntAdd(){
     Bag<int> bagInt;
 
     bagInt.add(12);
@@ -23,7 +28,7 @@ void BagTest::testAdd(){
     CPPUNIT_ASSERT_EQUAL(bagExpect, bagInt);
 }
 
-void BagTest::testRemove(){
+void BagTest::testIntRemove(){
     Bag<int> bagInt({26, 27});
 
     bagInt.remove(27);
@@ -33,12 +38,54 @@ void BagTest::testRemove(){
     CPPUNIT_ASSERT_EQUAL(bagExpect, bagInt);
 }
 
-void BagTest::testForeach(){
+void BagTest::testIntForeach(){
     Bag<int> bagInt({92, 63, 5});
 
     int count = 0;
     bagInt.foreach([&count](int i){
         count += i;
+    });
+    CPPUNIT_ASSERT_EQUAL(160, count);
+}
+
+void BagTest::testDummyInit(){
+    Bag<Dummyclass> bagA({Dummyclass("dummy1"), Dummyclass("dummy2")});
+    Bag<Dummyclass> bagB({Dummyclass("dummy1"), Dummyclass("dummy2")});
+    CPPUNIT_ASSERT(&bagA != &bagB);
+    CPPUNIT_ASSERT_EQUAL(bagA, bagB);
+
+    Bag<Dummyclass> bagC({Dummyclass("salut"), Dummyclass("hello")});
+    CPPUNIT_ASSERT(!(bagA == bagC));
+}
+
+void BagTest::testDummyAdd(){
+    Bag<Dummyclass> bagDummy;
+
+    bagDummy.add(Dummyclass("hello"));
+    bagDummy.add(Dummyclass("13"));
+    bagDummy.add(Dummyclass("26"));
+    CPPUNIT_ASSERT_EQUAL(3, bagDummy.size());
+
+    Bag<Dummyclass> bagExpect({Dummyclass("hello"), Dummyclass("13"), Dummyclass("26")});
+    CPPUNIT_ASSERT_EQUAL(bagExpect, bagDummy);
+}
+
+void BagTest::testDummyRemove(){
+    Bag<Dummyclass> bagDummy({Dummyclass("26"), Dummyclass("27")});
+
+    bagDummy.remove(Dummyclass("27"));
+    CPPUNIT_ASSERT_EQUAL(1, bagDummy.size());
+
+    Bag<Dummyclass> bagExpect({Dummyclass("26")});
+    CPPUNIT_ASSERT_EQUAL(bagExpect, bagDummy);
+}
+
+void BagTest::testDummyForeach(){
+    Bag<Dummyclass> bagDummy({Dummyclass("92"), Dummyclass("63"), Dummyclass("5")});
+
+    int count = 0;
+    bagDummy.foreach([&count](const Dummyclass& dummy){
+        count += std::stoi(dummy.getName());
     });
     CPPUNIT_ASSERT_EQUAL(160, count);
 }
--- test/bagTest.hpp
+++ test/bagTest.hpp
@@ -2,24 +2,71 @@
 #include <cppunit/TestFixture.h>
 #include <cppunit/TestSuite.h>
 #include <cppunit/extensions/HelperMacros.h>
+#include <cstddef>
+#include <functional>
+#include <ostream>
+#include <string>
 #include "../src/utils/bag.hpp"
 
 
 class BagTest : public CppUnit::TestFixture{
 CPPUNIT_TEST_SUITE(BagTest);
-CPPUNIT_TEST(testInit);
-CPPUNIT_TEST(testAdd);
-CPPUNIT_TEST(testRemove);
-CPPUNIT_TEST(testForeach);
+    CPPUNIT_TEST(testIntInit);
+    CPPUNIT_TEST(testIntAdd);
+    CPPUNIT_TEST(testIntRemove);
+    CPPUNIT_TEST(testIntForeach);
+
+    CPPUNIT_TEST(testDummyInit);
+    CPPUNIT_TEST(testDummyAdd);
+    CPPUNIT_TEST(testDummyRemove);
+    CPPUNIT_TEST(testDummyForeach);
 CPPUNIT_TEST_SUITE_END();
 
 public:
-    void testInit();
-    void testAdd();
-    void testRemove();
-    void testForeach();
+    void testIntInit();
+    void testIntAdd();
+    void testIntRemove();
+    void testIntForeach();
+
+    void testDummyInit();
+    void testDummyAdd();
+    void testDummyRemove();
+    void testDummyForeach();
 
 };
 CPPUNIT_TEST_SUITE_REGISTRATION(BagTest);
 
 template class Bag<int>;
+
+class Dummyclass{
+
+private:
+    std::string name;
+
+public:
+    Dummyclass(std::string name):name(name){
+
+    }
+
+    const std::string& getName() const{
+        return name;
+    }
+
+    bool operator==(const Dummyclass& other) const{
+        return this->name == other.name;
+    }
+
+};
+
+std::ostream& operator<<(std::ostream& stream, const Dummyclass& dummy);
+
+namespace std {
+    template<>
+    struct hash<Dummyclass>{
+        size_t operator()(const Dummyclass& object) const{
+            return std::hash<std::string>{}(object.getName());
+        }
+    };
+}
+
+template class Bag<Dummyclass>;
